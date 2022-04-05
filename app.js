aeaf248566aa4f1b585139b33e7719f046612eb0
sherryrich <ricsherry@gmail.com>
2022-04-05 12:01:14 +0000

    both dice are returning random numbers

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-let randomNumber1 = Math.floor((Math.random() * 6) + 1);
-console.log(randomNumber1);
+let randomNumber1 = Math.floor((Math.random() * 6) + 1); //random no between 1 & 6
 
-randomNumber1 = document.getElementsByClassName("img1").setAttribute("/dice1.png"); 
-document.getElementsByClassName("img1").src = "dice1.png";
+let randomDiceImage = "dice" + randomNumber1 + ".png";  //dice1.png = dice6.png
+
+let randomImageSource = "images/" + randomDiceImage;  //images/dice.png - images/dice6.png
+
+let image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource)
+
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2)
