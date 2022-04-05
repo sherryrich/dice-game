d392e48c2f525a1e82e9fa64a6eebcab4830c1f6
sherryrich <ricsherry@gmail.com>
2022-04-05 11:48:14 +0000

    styling updated

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
-console.log("connected");
-alert("connected");
+let randomNumber1 = Math.floor((Math.random() * 6) + 1);
+console.log(randomNumber1);
+
+randomNumber1 = document.getElementsByClassName("img1").setAttribute("/dice1.png"); 
+document.getElementsByClassName("img1").src = "dice1.png";
